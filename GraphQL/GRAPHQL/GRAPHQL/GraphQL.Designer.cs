b56f8371711cdcfa2c8ba783be7683a776c7136e
LongNguyen1101 <longnguyenhoang1101.work@gmail.com>
2023-04-22 13:36:52 +0700

    Add graphql taking rickandmortyapi

---
+++ GraphQL/GRAPHQL/GRAPHQL/GraphQL.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GRAPHQL
+{
+    partial class GRAPHQLFORM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGet = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dgvShow = new System.Windows.Forms.DataGridView();
+            this.columnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGet
+            // 
+            this.btnGet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGet.Location = new System.Drawing.Point(508, 406);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(386, 93);
+            this.btnGet.TabIndex = 0;
+            this.btnGet.Text = "Get Rick and Morty API";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // dgvShow
+            // 
+            this.dgvShow.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvShow.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnName});
+            this.dgvShow.Location = new System.Drawing.Point(12, 12);
+            this.dgvShow.Name = "dgvShow";
+            this.dgvShow.RowHeadersWidth = 62;
+            this.dgvShow.RowTemplate.Height = 28;
+            this.dgvShow.Size = new System.Drawing.Size(431, 899);
+            this.dgvShow.TabIndex = 1;
+            // 
+            // columnName
+            // 
+            this.columnName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.columnName.HeaderText = "Name";
+            this.columnName.MinimumWidth = 8;
+            this.columnName.Name = "columnName";
+            this.columnName.ReadOnly = true;
+            // 
+            // GRAPHQLFORM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 923);
+            this.Controls.Add(this.dgvShow);
+            this.Controls.Add(this.btnGet);
+            this.Name = "GRAPHQLFORM";
+            this.Text = "GraphQL";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvShow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGet;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dgvShow;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnName;
+    }
+}
+
---
+++ GraphQL/GRAPHQL/GRAPHQL/GraphQL.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using GraphQL.Client.Abstractions;
+using System.Collections;
+using System.Net.Http;
+using System.Windows.Forms.VisualStyles;
+using Newtonsoft.Json;
+
+namespace GRAPHQL
+{
+    public partial class GRAPHQLFORM : Form
+    {
+        private readonly GraphQLHttpClient _httpClient;
+
+        public GRAPHQLFORM()
+        {
+            InitializeComponent();
+            _httpClient = new GraphQLHttpClient("https://rickandmortyapi.com/graphql", new NewtonsoftJsonSerializer());
+        }
+
+        public class RickandMortyApi
+        {
+            public CharacterList characters { get; set; }
+        }
+
+        public class CharacterList
+        {
+            public info info { get; set; }
+            public results[] results { get; set; }
+        }
+        public class info
+        {
+            public int count { get; set; }
+        }
+        public class results
+        {
+            public string name { get; set; }
+        }
+
+        private async void btnGet_Click(object sender, EventArgs e)
+        {
+            var graphQLRequest = new GraphQLRequest
+            {
+                Query = @"
+                    query {
+                        characters (page: 2, filter: { name: ""rick"" }) {
+                            info { count }
+                            results { name }
+                        }
+                    }"
+            };
+
+            var graphQLResponse = await _httpClient.SendQueryAsync<RickandMortyApi>(graphQLRequest);
+
+            if (graphQLResponse.Errors != null)
+            {
+                MessageBox.Show("Error for receive API!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                var data = graphQLResponse.Data;
+                var results = data.characters.results;
+                dgvShow.Rows.Clear();
+
+                foreach (var result in results)
+                {
+                    var name = result.name.ToString();
+                    dgvShow.Rows.Add(name);
+                }
+            }
+
+        }
+    }
+}
